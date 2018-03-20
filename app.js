23b670b28f3496fab5bc8c1274343c5ab16f1a91
swiftyy188 <jeffho.ml@gmail.com>
2018-03-20 15:07:32 -0400

    Mongoose added - npm install bluebird mongoose -save

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 
 var app = express();
+var router = express.Router();
 
 //bodyParser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({'extended': 'false'}));
+
+
 
 //catch 404 and forward to error handler
 app.use(function(req, res, next){
